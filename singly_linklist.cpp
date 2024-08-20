dd8e502cf35d9effbdf67be171ee4f34f13d67fc
Chetan Kolhe <Chetankolhe1020@gmail.com>
2024-08-20 23:08:50 +0530

    20.08.2024

--- singly_linklist.cpp
+++ singly_linklist.cpp
@@ -78,7 +78,7 @@ void createloop(node* head, int loopstart){
 void display(node* &head){
     node* temp=head;
     if(head==NULL){
-       cout<<"Link list empty"; 
+       cout<<"Link listf empty"; 
     }
     else{
         while(temp!=NULL){
