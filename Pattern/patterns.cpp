f4df6271a2b70d32492dc211655e94d55153daad
Chetan Kolhe <Chetankolhe1020@gmail.com>
2024-08-21 00:44:09 +0530

    20.08.2024

--- Pattern/patterns.cpp
+++ Pattern/patterns.cpp
@@ -1,6 +1,124 @@
 #include<iostream>
 using namespace std;
+
+void pattern1(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+
+    }
+}
+void pattern2(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+
+    }
+}
+void pattern3(int n){
+    for(int i=0;i<n;i++){
+
+            for(int k=0;k<i;k++){
+                cout<<"  ";
+                
+            }
+            for(int j=0;j<n-i;j++){
+                cout<<"* ";
+             }
+             cout<<endl;
+        }
+        
+
+}
+
+void pattern4(int n){
+    for(int i=0;i<n;i++){
+
+        for(int j=n ;j>i;j--){
+            cout<<"  ";
+        }
+        for(int k=0;k<i+1;k++){
+            cout<<"* ";
+        }
+        cout<<endl;
+        
+
+    }
+}
+
+
+void pattern5(int n){
+    for(int i=n;i>0;i--){
+        for(int j=i;j>0;j--){
+            cout<<"* ";
+        }
+        cout<<endl;
+
+    }
+}
+
+void pattern7(int n){
+    for(int i=0;i<n;i++){
+
+        for(int j=n ;j>i;j--){
+            cout<<"  ";
+        }
+        for(int k=0;k<i+1;k++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        
+
+    }
+}
+
+void pattern6(int n){
+    for(int i=n;i>0;i--){
+        for(int j=i;j>0;j--){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+
+    }
+}
+
 int main(){
-    cout<<"wiell df do it";
+
+
+    //20.08.2024
+    int choice;
+    cout<<"Enter the pattern Number :- 1 to 9 "<<endl;
+    cin>>choice;
+    switch(choice){
+        case 1:
+        pattern1(5);
+        break;
+        case 2:
+        pattern2(5);
+        break;
+        case 3:
+        pattern3(5);
+        break;
+        case 4:
+        pattern4(5);
+        break;
+        case 5:
+        pattern5(5);
+        break;
+        case 6:
+        pattern6(5);
+        break;
+        case 7:
+        pattern7(5);
+        break;
+        default:
+        cout<<"Enter the correct choice"<<endl;
+
+    }
     
+    ;
 }
